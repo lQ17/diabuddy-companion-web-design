41f9b1423bdf40b0b8e46a6d30ebb99be3e9c5a6
YangQX <1370655310@qq.com>
2024-01-04 16:57:47 +0800

    完成自动按需导入vant-ui以及响应式自动适配浏览器(PostCSS)

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,17 @@
 
 <template>
   <div>
+    <div class="box"></div>
     Hello Vue
+    <van-button type="primary">主要按钮</van-button>
     <router-view></router-view>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.box {
+  height: 300px;
+  width: 300px;
+  background-color: pink;
+}
+</style>
